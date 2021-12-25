296f65ae30a00e7eddfe6f44a49796e57cda6efe
vidushalakshan <vidushalakshan7890@gmail.com>
2021-12-25 18:24:01 +0530

    added loginform and dashboard view

---
+++ src/controller/DashBoardFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class DashBoardFormController {
+    public void RegisterStudentOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void TrainingProgramsOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void StudentDetailsOnAction(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/controller/LoginFromController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class LoginFromController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassWord;
+
+    public void AdminLoginOnAction(ActionEvent actionEvent) {
+
+    }
+}

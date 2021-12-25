16c6a7a3060abf20d79c4c88e0106085a41f9338
vidushalakshan <vidushalakshan7890@gmail.com>
2021-12-25 22:21:30 +0530

    added StudentDetailsForm view

---
+++ src/controller/StudentDetailsFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class StudentDetailsFormController {
+    public TableView tblStudentDetails;
+    public TableColumn colRegisterNumber;
+    public TableColumn colStudentName;
+    public TableColumn colStudentNumber;
+    public TableColumn colProgram;
+    public TableColumn colDuration;
+    public TableColumn colFee;
+
+    public void btnDelete(ActionEvent actionEvent) {
+    }
+}

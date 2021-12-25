f5416bea3ac5ae8cbf4725c94075b62210899871
vidushalakshan <vidushalakshan7890@gmail.com>
2021-12-25 21:53:55 +0530

    added Training Program view

---
+++ src/controller/TrainingProgramFormController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class TrainingProgramFormController {
+    public JFXTextField txtProgramID;
+    public JFXTextField txtProgram;
+    public JFXTextField txtDuration;
+    public JFXTextField txtFee;
+    public TableView tblTrainingProgram;
+    public TableColumn colProgramID;
+    public TableColumn colProgram;
+    public TableColumn colDuration;
+    public TableColumn colFee;
+
+    public void btnAddTraining(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdate(ActionEvent actionEvent) {
+    }
+}
